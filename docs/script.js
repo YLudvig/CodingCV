e11c5010c7d3b761736dffd0572b4a71142cc4c5
YLudvig <yslu24ne@student.ju.se>
2025-01-22 14:07:28 +0100

    jsonfil

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,26 @@
 console.log('Hej från JS');
+
+async function fetchCV() {
+    try {
+        const url = 'https://yludvig.github.io/CodingCV/';
+
+
+        const competencies = await fetch(url); 
+        const data = await competencies.json();
+
+        console.log(data);
+
+    } catch (error) {
+        console.error('Error fetching user data: ', error);
+    }
+}
+
+fetchCV();
+
+async function updateCV() {
+    try {
+        
+    } catch (error) {
+        console.error('Error fetching user data: ', error);
+    }
+}
